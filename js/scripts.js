e084a8f762e4a945dcb7176032f72f1be77a4238
stwilliams0223 <swilli46@engr-134-02.eastern.ewu.edu>
2015-03-09 14:04:09 -0700

    max clouds

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ options = {
   src: 'img/new-cloud.png', // path to image, the default is a base64 (you can see the actual string in sources)
   maxWidth: 300, // max image's width
   minWidth: 200,
-  maxHeight: 96, // amx image's height
+  maxHeight: 200, // amx image's height
   minClouds: 10, // minimum amount of clouds
   maxClouds: 10, // maximum amount of clouds
   skyHeight: null, // height of the container element
