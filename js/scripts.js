61c412cd5da5599056ae420ba675effb77b5bcb6
stwilliams0223 <swilli46@engr-134-02.eastern.ewu.edu>
2015-03-09 13:23:18 -0700

    base commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+
+
+options = {
+  src: 'img/new-cloud.png', // path to image, the default is a base64 (you can see the actual string in sources)
+  maxWidth: 300, // max image's width
+  maxHeight: 96, // amx image's height
+  minClouds: 10, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
+  skyHeight: null, // height of the container element
+  wind: true // animate clouds, default is false
+};
+
+$('#sky').jQlouds(options);
